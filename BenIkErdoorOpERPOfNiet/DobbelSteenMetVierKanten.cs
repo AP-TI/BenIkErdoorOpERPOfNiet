3cdca9061f560b1a0baab4f2ee437b8e69909026
mixxamm <maximjanssensj5@gmail.com>
2020-01-15 21:30:12 +0100

    Add project files.

---
+++ BenIkErdoorOpERPOfNiet/DobbelSteenMetVierKanten.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BenIkErdoorOpERPOfNiet
+{
+    class DobbelSteenMetVierKanten
+    {
+        Random random;
+        public DobbelSteenMetVierKanten()
+        {
+            random = new Random();
+        }
+
+        public int Gooi()
+        {
+            return random.Next(1, 5);
+        }
+    }
+}
---
+++ BenIkErdoorOpERPOfNiet/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BenIkErdoorOpERPOfNiet
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int AANTAL_TESTEN = 1000;
+            int aantalGeslaagd = 0;
+            for(int i = 0; i < AANTAL_TESTEN; i++)
+            {
+                if (Test(15))
+                    aantalGeslaagd++;
+            }
+            Console.WriteLine($"Je bent op {aantalGeslaagd} van de {AANTAL_TESTEN} testen geslaagd.");
+        }
+
+        private static bool Test(int aantalVragen)
+        {
+            DobbelSteenMetVierKanten juistAntwoord = new DobbelSteenMetVierKanten();
+            DobbelSteenMetVierKanten gekozenAntwoord = new DobbelSteenMetVierKanten();
+            int juistTeller = 0;
+            for (int i = 0; i < aantalVragen; i++)
+            {
+                int juistAntwoord1 = juistAntwoord.Gooi();
+                int gekozenAntwoord1 = gekozenAntwoord.Gooi();
+                if (juistAntwoord1 == gekozenAntwoord1)
+                    juistTeller++;
+                Console.WriteLine(juistAntwoord1 + " " + gekozenAntwoord1);
+            }
+            return juistTeller >= aantalVragen / 2.0;
+        }
+    }
+}
